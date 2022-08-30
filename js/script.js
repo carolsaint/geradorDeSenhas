f0dae3e6d07b67cde21711a145eff7c740602e71
Carol Saint <santocarolina98@gmail.com>
2022-08-30 15:54:21 -0300

    functions to get characters

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const generatePasswordButton = document.querySelector("#generate-password")
 const generatedPasswordElement = document.querySelector("#generated-password")
 
 //Funções
+//letras, numeros e simbolos
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=<>,.!@#$%&*+-"
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 //Eventos
 generatePasswordButton.addEventListener("click", () =>{
