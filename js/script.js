24dcd3edda48198cf75ab68f542d9341e6130d24
Carol Saint <santocarolina98@gmail.com>
2022-08-30 15:40:25 -0300

    js selection and event

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password")
+const generatedPasswordElement = document.querySelector("#generated-password")
+
+//Funções
+
+
+//Eventos
+generatePasswordButton.addEventListener("click", () =>{
+    console.log("teste")
+})
